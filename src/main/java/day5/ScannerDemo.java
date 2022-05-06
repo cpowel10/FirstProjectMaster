143487d3c09799f25ca6eab6aef2b132e8e208fd
Chris Powell <chrispowell1129@gmail.com>
2022-05-06 11:31:09 -0400

    ScannerDemo created

---
+++ src/main/java/day5/ScannerDemo.java
@@ -0,0 +1,18 @@
+package day5;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+    public static void main(String[] args) {
+        String name = null;
+        int age = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name: ");
+        name = scanner.next(); //wait for user input
+        System.out.println("Please enter your age: ");
+        age = scanner.nextInt();
+
+        System.out.println("Welcome: "+name+" and your age is: "+age);
+    }
+}
