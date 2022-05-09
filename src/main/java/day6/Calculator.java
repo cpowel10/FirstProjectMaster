12f16303ef3729f1892df6b47d01ee3179e30bd3
Chris Powell <chrispowell1129@gmail.com>
2022-05-09 15:28:19 -0400

    JUnit Calculator methods complete

---
+++ src/main/java/day6/Calculator.java
@@ -0,0 +1,11 @@
+package day6;
+
+public class Calculator {
+    public int sum(int num1, int num2){
+        return num1+num2;
+    }
+
+    public int subtract(int num1,int num2){
+        return num1-num2;
+    }
+}
